81dbc3d6979db62791759cd4b012ba7e50bfbb27
Ismail <ismailsafadi@gmail.com>
2018-09-22 10:23:41 +0300

    Initial Commit

---
+++ TestHomeIsmail2018Tests/TestHomeIsmail2018Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TestHomeIsmail2018Tests.swift
+//  TestHomeIsmail2018Tests
+//
+//  Created by Ismail on 9/22/18.
+//  Copyright © 2018 Ismail. All rights reserved.
+//
+
+import XCTest
+@testable import TestHomeIsmail2018
+
+class TestHomeIsmail2018Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
